6f8ffc8b3b03fd4b3a62ef3f59f487fcd86358f1
Matthias Aznar-Schwarz <m.aznar.schwarz@gmail.com>
2021-08-29 19:26:20 +0200

    front of Signin Page done

--- frontend-vue/src/App.vue
+++ frontend-vue/src/App.vue
@@ -1,10 +1,23 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/signin">Sign In</router-link>
   </div>
   <router-view/>
 </template>
+
+<script>
+import { RouterView, RouterLink } from "vue-router";
+
+export default {
+  name: "App",
+  components: {
+    RouterView,
+    RouterLink
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
@@ -17,6 +30,7 @@
 
 #nav {
   padding: 30px;
+  background-color: whitesmoke;
 
   a {
     font-weight: bold;
---
+++ frontend-vue/src/views/Signin.vue
@@ -0,0 +1,128 @@
+<template>
+
+    <article id="articleSignin">
+        <form class="formSignin">
+            <h3 class="formSignin__h3">Sign In</h3>
+
+            <div class="formSignin__div">
+                <input type="email" placeholder="Email address" class="formSignin__input" />
+            </div>
+
+            <div class="formSignin__div">
+                <input type="password" placeholder="Password" class="formSignin__input" />
+            </div>
+
+            <button type="submit" class="formSignin__btt">Connexion</button>
+
+            <p class="formSignin__p formSignin__p--barre">
+                OU
+            </p>
+
+            <p  class="formSignin__p">
+                <router-link to="/forgot-password">Mot de passe oublié ?</router-link>
+            </p>
+        </form>
+    </article>
+
+</template>
+
+<script>
+export default {
+    name: "Signin",
+}
+</script>
+
+<style lang="scss">
+
+*, *:before, *:after {
+  box-sizing: border-box;
+}
+
+#articleSignin {
+    background-color: whitesmoke;
+
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    //background-image: url("./frontend-vue/public/images/icon.png");
+    //background-position: center;
+}
+
+.formSignin{
+    background-color: white;
+    width: 350px;
+
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+
+    border: lightgray;
+    border-style: solid;
+    border-width: 1px;
+    
+    &__h3{
+        margin: 25px auto 25px auto ;  // top right bottom left
+        font-size: 3em;
+    }
+    &__div{
+        margin: 0px auto 0px auto ;  // top right bottom left
+    }
+    &__input{
+        width: 280px; height: 30px;
+        margin: 5px 0px 5px 0px ; // top right bottom left
+        padding: 0px 0px 0px 7px;   // top right bottom left
+
+        border: lightgray;
+        border-style: solid;
+        border-width: 1px;
+        background-color: whitesmoke;
+    }
+    &__btt{
+        width: 280px; height: 30px;
+        margin: 10px auto 0px auto ;  // top right bottom left
+
+        border-radius: 5px;
+        border: none;
+        background-color: rgb(180, 215, 252);
+
+        cursor: pointer;
+    }
+    &__p{
+        margin: 15px auto 15px auto ;  // top right bottom left
+
+        a{
+            text-decoration: none;
+            display: block;
+            color: black;
+            cursor: pointer;
+            transition: transform 0.5s;
+
+            &:hover{
+                color: blue;
+                transform: scale(1.1);
+            }
+        }
+
+        &--barre{
+            display: flex;
+            align-items: center;
+
+            &::before{
+                content: '';
+                display: block;
+                width: 100px; height: 2px;
+                background-color: whitesmoke;
+                margin-right: 10px;
+            }
+            &::after{
+                content: '';
+                display: block;
+                width: 100px; height: 2px;
+                background-color: whitesmoke;
+                margin-left: 10px;
+            }
+        }
+    }
+}
+
+</style>
