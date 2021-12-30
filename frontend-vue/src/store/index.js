0ccc3e2fe12d33612559883e4d1c29b2faad0c73
Matthias Aznar-Schwarz <m.aznar.schwarz@gmail.com>
2021-12-30 18:10:29 +0100

    deletion of some useless things

--- frontend-vue/src/store/index.js
+++ frontend-vue/src/store/index.js
@@ -10,21 +10,6 @@ export default createStore({
     userPicture : "",
     UserPublications : 35,
     UserFriends : 8,
-    Posts : [{
-      Post_ID : '00001',
-      Person_ID : 'Kaerbran',
-      Post_location : 'Paris, 14ème arrondissement',
-      Post_picture : 'black-fox-silhouette.jpg',
-      Post_comment : 'ipsum dolor sit amet consectetur adipisicing elit. Ipsam atque recusandae amet eligendi! Suscipit tempora laudantium quidem, laborum totam dolores, cumque ex blanditiis fuga voluptates eaque sapiente necessitatibus, quod repellendus.',
-      Post_Date_Published : 'Apr 12 2018'
-    }, {
-      Post_ID : '00002',
-      Person_ID : 'Kaerbran',
-      Post_location : 'Paris, 15ème arrondissement',
-      Post_picture : 'black-fox-silhouette.jpg',
-      Post_comment : 'ipsum dolor sit amet consectetur adipisicing elit. Ipsam atque recusandae amet eligendi! Suscipit tempora laudantium quidem, laborum totam dolores, cumque ex blanditiis fuga voluptates eaque sapiente necessitatibus, quod repellendus.',
-      Post_Date_Published : 'Apr 15 2018'
-    }]
   },
   mutations: {
     userAuthentification (state, payload) {
